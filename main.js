7c77ff34b9f3a9a7f3a22378a2d74c1be18ab744
Igor Knyazev <igorknyazev@Igors-MacBook-Pro.local>
2022-08-19 15:10:34 +0300

    create sphere

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
   innerWidth / innerHeight,
   0.1,
   1000
-)
+);
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(innerWidth, innerHeight)
-document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// create a sphere
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+  })
+);
+scene.add(sphere);
+
+camera.position.z = 10
 
 function animate() {
-  requestAnimationFrame(animate)
-  renderer.render(scene, camera)
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
-animate()
+animate();
